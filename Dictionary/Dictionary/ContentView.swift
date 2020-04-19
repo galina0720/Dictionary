3dbbf4f7a8a9092a43131725bb9a91781a0d2aef
galina0720 <gujialing0720@163.com>
2020-04-19 13:58:11 +0800

    first commit

---
+++ Dictionary/Dictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dictionary
+//
+//  Created by galina on 19/4/20.
+//  Copyright © 2020 galina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
